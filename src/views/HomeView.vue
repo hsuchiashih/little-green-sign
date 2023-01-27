33deefca96aa298c3a700a0954b6d578e7da760e
hsuchiashih <hsuchiashih@gmail.com>
2023-01-27 15:11:26 +0800

    vue3 + vite + bs5 基本配置

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main> 
+    <button type="button" class="btn btn-primary">Primary</button>
+  </main>
+</template>
